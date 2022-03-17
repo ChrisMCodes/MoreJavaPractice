e1d173eecd8871374ddbfd277193ecdf01e71c2e
ChrisMCodes <34985965+ChrisMCodes@users.noreply.github.com>
2022-03-16 20:06:49 -0700

    Add files via upload

---
+++ Pool/Rectangle.java
@@ -0,0 +1,22 @@
+public class Rectangle {
+    // write your code here
+    private double width;
+    private double length;
+    
+    public Rectangle(double width, double length) {
+        this.width = (width >= 0) ? width:0;
+        this.length = (length >= 0) ? length:0;
+    }
+    
+    public double getWidth() {
+        return this.width;
+    }
+    
+    public double getLength() {
+        return this.length;
+    }
+    
+    public double getArea() {
+        return this.width * this.length;
+    }
+}
